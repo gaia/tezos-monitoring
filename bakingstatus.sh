21da666d61a497054e6806cc6f5f9756c278b979
gaia <gaia@users.noreply.github.com>
2018-07-30 12:01:20 -0700

    Create bakingstatus.sh

---
+++ bakingstatus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Feel free to use and modify this script, but please give credit to
+# https://github.com/gaia/tezos-monitoring/
+
+# This script tells you if your baker is running. It doesn't mean the baker is runnning as it should,
+# only that the process is running. It could be stuck.
+
+tezosuser="tezos"
+
+# Do not modify anything from here down
+
+nodestatus=$(ps -fU $tezosuser | grep "[t]ezos-baker")
+echo "$nodestatus" | grep -qi "tezos-baker"
+if [ $? -eq 0 ];then
+        echo "Baking..."
+elif [[ -n "$flag" ]];then
+        echo "Baking Stopped"
+fi
